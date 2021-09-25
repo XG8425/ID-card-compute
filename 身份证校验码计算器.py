058215c3428d70b0a82a388b7030870e6034a8bd
XG8425 <81908797+XG8425@users.noreply.github.com>
2021-09-25 10:59:06 +0800

    Add files via upload

---
+++ 身份证校验码计算器.py
@@ -0,0 +1,43 @@
+#!usr/bin/python3
+
+a=3
+b=5
+c=0
+d=2
+e=0
+f=4
+g=1
+h=9
+i=9
+j=8
+k=0
+l=5
+m=2
+n=3
+o=1
+p=8
+q=2
+r=a*7+b*9+c*10+d*5+e*8+f*4+g*2+h*1+i*6+j*3+k*7+l*9+m*10+n*5+o*8+p*4+q*2
+s=r%11
+if s==0:
+    print (1)
+elif s==1:
+    print (0)
+elif s==2:
+    print (X)
+elif s==3:
+    print (9)
+elif s==4:
+    print (8)
+elif s==5:
+    print (7)
+elif s==6:
+    print (6)
+elif s==7:
+    print (5)
+elif s==8:
+    print (4)
+elif s==9:
+    print (3)
+elif s==10:
+    print (2)
